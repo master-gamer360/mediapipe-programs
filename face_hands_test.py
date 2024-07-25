8299ec5d0732e63d37a06565676dbe0f8d5ec998
Programmer Penguin <129675740+master-gamer360@users.noreply.github.com>
2024-07-26 00:24:43 +0500

    Create face_hands_test.py

---
+++ face_hands_test.py
@@ -0,0 +1,62 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+def main():
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_drawing = mp.solutions.drawing_utils
+
+    # Define custom drawing specifications
+    drawing_spec = mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2)  # Green color for nodes
+    connection_spec = mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2)  # Red color for connections
+
+    # Finger tip landmarks
+    finger_tips = [mp_hands.HandLandmark.THUMB_TIP, mp_hands.HandLandmark.INDEX_FINGER_TIP,
+                mp_hands.HandLandmark.MIDDLE_FINGER_TIP, mp_hands.HandLandmark.RING_FINGER_TIP,
+                mp_hands.HandLandmark.PINKY_TIP]
+
+
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1)  # Set max_num_faces to 1 for single face tracking
+    cap = cv2.VideoCapture(0)  # Open the default camera (usually webcam)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert BGR frame to RGB for MediaPipe
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        blank_image = np.zeros_like(rgb_frame)
+
+        face_results = face_mesh.process(rgb_frame)
+        hand_results = hands.process(rgb_frame)
+
+        if face_results.multi_face_landmarks:
+            for face_landmarks in face_results.multi_face_landmarks:
+                # Draw face landmarks on the frame
+                for landmark in face_landmarks.landmark:
+                    x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+                    cv2.circle(blank_image, (x, y), 2, (50, 50, 50), -1)
+
+        if hand_results.multi_hand_landmarks:
+            for hand_landmarks in hand_results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    blank_image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                    drawing_spec, connection_spec
+                )
+
+
+
+        cv2.imshow("Face Mesh", frame)
+        cv2.imshow("fake stuff", blank_image)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
